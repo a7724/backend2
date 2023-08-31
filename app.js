d998615f939362a10bea2fc1577c1d4310b88812
unknown <yuhsianglan.28@gmail.com>
2023-08-31 13:20:16 +0800

    f

--- app.js
+++ app.js
@@ -1,22 +1,75 @@
+// const express = require("express");
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.json({
+//     mesg: "hello",
+//   });
+// });
+
+// app.get("/users", (req, res) => {
+//   res.json({
+//     mesg: "hello users",
+//   });
+// });
+
+// app.get("/orders", (req, res) => {
+//   res.json({
+//     mesg: "hello orders",
+//   });
+// });
+
+// app.listen(3000);
+
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookieParser");
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import orderRoute from "./routes/order.js";
+
 const app = express();
+dotenv.config();
 
-app.get("/", (req, res) => {
-  res.json({
-    mesg: "hello",
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connect to db");
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/auth", authRoute);
+app.use("/users", usersRoute);
+app.use("/hotels", hotelsRoute);
+app.use("/rooms", roomsRoute);
+app.use("/order", orderRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
   });
 });
 
-app.get("/users", (req, res) => {
-  res.json({
-    mesg: "hello users",
-  });
+app.listen(3000, () => {
+  connect();
+  console.log("Connected to backend.");
 });
-
-app.get("/orders", (req, res) => {
-  res.json({
-    mesg: "hello orders",
-  });
-});
-
-app.listen(3000);
